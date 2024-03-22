4ee68fb95db7b9d6684f452d7a47702ecb407432
victormuteithia <muteithiavictor@gmail.com>
2024-03-22 03:08:45 +0300

    Completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,23 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resourses", "Building C, East Wing")
+print(hr)
+
+hr.name = "HR"
+hr.location = "Building F, 10th Floor"
+print(hr)
+hr.update()
+print(hr)
+
+print("Delete Payroll")
+payroll.delete()
+print(payroll)
+
 ipdb.set_trace()
